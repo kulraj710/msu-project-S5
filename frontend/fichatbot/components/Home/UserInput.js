8132168e8eacf544fd950614c88bbced6a79d1a7
Pratik Luhar <pratikluhar022@gmail.com>
2023-08-02 21:34:40 +0530

    Userinput

--- frontend/fichatbot/components/Home/UserInput.js
+++ frontend/fichatbot/components/Home/UserInput.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import IconButton from "@mui/material/IconButton";
 import SendIcon from '@mui/icons-material/Send';
 import InputAdornment from '@mui/material/InputAdornment';
@@ -6,34 +6,31 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import styles from "../../styles/Home/Input.module.css"
 // import Divider from '@mui/material/Divider';
 
-const UserInput = ({chatArray, setChatArray}) => {
+const UserInput = ({ chatArray, setChatArray }) => {
 
     const [userInput, setUserInput] = useState("")
 
     const submitHandler = (e) => {
         e.preventDefault()
-        setChatArray([...chatArray, {id : 2, message : userInput, time : new Date(), sender : 0}])
+        setChatArray([...chatArray, { id: 2, message: userInput, time: new Date(), sender: 0 }])
         setUserInput("")
     }
 
     return (
         <div className={styles.container}>
-
-
-        
             <div>
-            <form onSubmit={submitHandler}>
-                <OutlinedInput
-                    placeholder='Enter text here'
-                    value={userInput}
+                <form onSubmit={submitHandler}>
+                    <OutlinedInput
+                        placeholder='Enter text here'
+                        value={userInput}
                     onChange={(e) => {setUserInput(e.target.value)}}
-                    fullWidth
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton type='submit'><SendIcon /></IconButton>
-                        </InputAdornment>}
-                />
-            </form>
+                        fullWidth
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton type='submit'><SendIcon /></IconButton>
+                            </InputAdornment>}
+                    />
+                </form>
             </div>
         </div>
     )
