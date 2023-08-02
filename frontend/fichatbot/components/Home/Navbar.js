cd95f716bf149770158165f85759983aa688a15c
Vishal Agrawal <137414169+Vishal-Agrawal63@users.noreply.github.com>
2023-08-02 19:43:29 +0530

    routes

--- frontend/fichatbot/components/Home/Navbar.js
+++ frontend/fichatbot/components/Home/Navbar.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import styles from "../../styles/Home/Navbar.module.css"
+import { auth } from "../../firebase.js"
 
 const Navbar = () => {
+  const fetchUser = () => {
+    const user = auth.currentUser;
+    if (user !== null) {
+        const displayName = user.displayName;
+        const email = user.email;
+        const uid = user.uid;
+        console.log("null")
+        // return { "name": displayName, "email:": email, "Uid": uid }
+
+    }
+    else {
+      console.log("null")
+        // return null;
+    }
+}
   return (
     <nav className={styles.navbar}>
       <div><h1>New Conversion</h1></div>
       <div>
         <ul>
-          <li id='btn1'><button>BTN1</button></li>
+          <li id='btn1'><button onClick={fetchUser}>BTN1</button></li>
           <li id='btn2'><button>BTN2</button></li>
           <li id='btn1'><button>BTN3</button></li>
         </ul>
--- frontend/fichatbot/firebase.js
+++ frontend/fichatbot/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvkgl8UPBnSHkI8LMqCN1QRJbrFZms2nM",
-  authDomain: "mini-project-auth-2886b.firebaseapp.com",
-  projectId: "mini-project-auth-2886b",
-  storageBucket: "mini-project-auth-2886b.appspot.com",
-  messagingSenderId: "1039421406280",
-  appId: "1:1039421406280:web:c3f10f0bfd3b854f5dec7d",
-  measurementId: "G-4TP8M99S69"
-};
+  apiKey: "AIzaSyBCYG26HqGC4pauHh8UBPxY7b47M8He7O0",
+  authDomain: "mini-project-auth-2a307.firebaseapp.com",
+  projectId: "mini-project-auth-2a307",
+  storageBucket: "mini-project-auth-2a307.appspot.com",
+  messagingSenderId: "394023318079",
+  appId: "1:394023318079:web:89b330c4be0fddeecb3e8a",
+  measurementId: "G-YEHTFYYK68"
+}
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth=getAuth();
