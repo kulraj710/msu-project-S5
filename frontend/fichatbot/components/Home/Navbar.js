8d288ce28d019c2423a86d693043dedce98b1143
kulraj710 <kulraj710@gmail.com>
2023-08-02 20:23:51 +0530

    Update : Auth session completed

--- frontend/fichatbot/components/Home/Navbar.js
+++ frontend/fichatbot/components/Home/Navbar.js
@@ -2,29 +2,23 @@ import React,{useEffect, useState} from 'react'
 import styles from "../../styles/Home/Navbar.module.css"
 import { auth } from "../../firebase.js"
 import Avatar from '@mui/material/Avatar'
-import { signOut  } from "firebase/auth"
+import { signOut, onAuthStateChanged   } from "firebase/auth"
+
 const Navbar = () => {
 
   const [currentUser, setCurrentUser] = useState(null)
 
 
-  // useEffect(() => {
-    
-  // }, [])
-
-function checkSession(){
-  const user = auth.currentUser;
-  if (user !== null) {
-      const displayName = user.displayName;
-      const email = user.email;
-      const uid = user.uid;
-      console.log(user.email)
-      setCurrentUser({ "name": displayName, "email" : email, "uid" : uid })
-  }
-  else{
-    console.log("Hello")
-  }
-}
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const displayName = user.displayName;
+        const email = user.email;
+        const uid = user.uid;
+        setCurrentUser({ "name": displayName, "email" : email, "uid" : uid })
+      } 
+    })
+  }, [])
 
   const logoutHandler = () => {
     signOut(auth).then(() => {
@@ -41,9 +35,8 @@ function checkSession(){
       <div>
         <ul>
           {/* <li id='btn1'><p>{(currentUser) ? currentUser.email : "You are not signed in!"}</p></li> */}
-          <li id='btn1'>{(currentUser) ? <Avatar sx={{ bgcolor: "lightblue" }}>{currentUser.name[0]}</Avatar> : "Please login"}</li>
+          <li id='btn1'>{(currentUser) ? <Avatar sx={{ bgcolor: "lightblue" }}>{currentUser.name[0]}</Avatar> : "NOT LOGGED IN"}</li>
           <li id='btn2'><button onClick={logoutHandler}>Logout</button></li>
-          <li id='btn1'><button onClick={checkSession}>Check</button></li>
         </ul>
       </div>
     </nav>
