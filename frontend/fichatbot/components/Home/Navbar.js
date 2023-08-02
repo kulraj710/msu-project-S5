2742ebeda557de1bcdaee36cd601711ebaafa262
kulraj710 <kulraj710@gmail.com>
2023-08-02 19:50:50 +0530

    update : Navbar auth state

--- frontend/fichatbot/components/Home/Navbar.js
+++ frontend/fichatbot/components/Home/Navbar.js
@@ -1,29 +1,28 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import styles from "../../styles/Home/Navbar.module.css"
 import { auth } from "../../firebase.js"
 
 const Navbar = () => {
-  const fetchUser = () => {
+
+  const [currentUser, setCurrentUser] = useState(null)
+
+
+  useEffect(() => {
     const user = auth.currentUser;
     if (user !== null) {
         const displayName = user.displayName;
         const email = user.email;
         const uid = user.uid;
-        console.log("null")
-        // return { "name": displayName, "email:": email, "Uid": uid }
+        setCurrentUser({ "name": displayName, "email:": email, "Uid": uid })
+    }
+  }, [])
 
-    }
-    else {
-      console.log("null")
-        // return null;
-    }
-}
   return (
     <nav className={styles.navbar}>
       <div><h1>New Conversion</h1></div>
       <div>
         <ul>
-          <li id='btn1'><button onClick={fetchUser}>BTN1</button></li>
+          <li id='btn1'><p>{(currentUser) ? currentUser.name : "You are not signed in!"}</p></li>
           <li id='btn2'><button>BTN2</button></li>
           <li id='btn1'><button>BTN3</button></li>
         </ul>
