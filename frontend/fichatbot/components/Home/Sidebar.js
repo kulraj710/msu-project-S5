189788023368c0024b6c820b353126c92045526f
Pratik Luhar <pratikluhar022@gmail.com>
2023-08-04 19:24:56 +0530

    Update: sidebar, globalcss, sidebar.module.css

--- frontend/fichatbot/components/Home/Sidebar.js
+++ frontend/fichatbot/components/Home/Sidebar.js
@@ -6,6 +6,9 @@ import Add from '../../public/icons/add.svg'
 import Logo from "../../public/full-logo.svg";
 
 const Sidebar = () => {
+
+  const newConv = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
   return (
     <div className={styles.container}>
       <section className={styles.section1}>
@@ -14,29 +17,34 @@ const Sidebar = () => {
           <div className={styles.supportingText}>Ask your finance questions</div>
         </div>
 
-      <div className={styles.logo}>
-      <Image src={Logo} width={50} height={50}/>
-      </div>
+        <div className={styles.logo}>
+          <Image src={Logo} width={50} height={50} />
+        </div>
       </section>
 
-      <section className={styles.section2}>
-        <div>
-          <h5>New Conversation</h5>
-          <span>2 messages</span>
-        </div>
+      <section className={styles.section2Container}>
+        {
+          newConv.map((each => (
+            <div className={styles.section2} key={each}>
+              <div>
+                <h5>New Conversation</h5>
+                <span>2 messages</span>
+              </div>
+            </div>)))
+        }
       </section>
 
       <section className={styles.section3}>
         <div className={styles.bottomFlexContainer}>
           <div>
-          <Image alt='Bot' src={Logo} width={30} height={30} className={styles.bottomImage1}/>
-          <Image alt='github logo' src={Github} width={25} height={25}/>
+            <Image alt='Bot' src={Logo} width={30} height={30} className={styles.bottomImage1} />
+            <Image alt='github logo' src={Github} width={25} height={25} />
           </div>
-        <div>
-          <button className={styles.newButton}>
-          <Image alt='logo' src={Add} width={20} height={20} className={styles.bottomImage1}/>
+          <div>
+            <button className={styles.newButton}>
+              <Image alt='logo' src={Add} width={20} height={20} className={styles.bottomImage1} />
 
-            <span>New Chat</span>
+              <span>New Chat</span>
             </button>
           </div>
         </div>
