f625f97ab33435c2953bf77ae4821e37eb4ec75c
kulraj710 <kulraj710@gmail.com>
2023-12-05 20:26:30 +0530

    Fixed Vercel named import issue

--- frontend/fichatbot/layouts/ButtonSecondary.jsx
+++ frontend/fichatbot/layouts/ButtonSecondary.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import s from "../styles/Layouts/ButtonSecondary.module.css"
-
+import s from "../styles/Layouts/BtnSecondary.module.css"
 
 const ButtonSecondary = ({ onClick, id, styles, type, label }) => {
     return (
